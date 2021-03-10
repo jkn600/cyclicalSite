087860fe96f27de1866473dcbd79ac54efd75174
jkn600 <nsubuga@live.co.uk>
2021-03-10 20:24:55 +0000

    More Responsiveness, Navbar menu changes, more placeholder text

---
+++ resources/js/script.js
@@ -0,0 +1,13 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("navBar");
+    var y = document.getElementById("header");
+    if (x.className === "topNav") {
+      x.className += " responsive";
+      y.style.height = "200px";
+    } else {
+      x.className = "topNav";
+      y.style.height = "100px";
+
+    }
+  }
